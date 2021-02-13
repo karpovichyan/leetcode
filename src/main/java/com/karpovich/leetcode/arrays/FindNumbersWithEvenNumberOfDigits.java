884178c32bf2db649a858231942adf90e0e0fdbf
karpovichyan <karpovichjan@gmail.com>
2021-02-13 15:38:47 +0300

    Introduction to Data Structure task#3

---
+++ src/main/java/com/karpovich/leetcode/arrays/FindNumbersWithEvenNumberOfDigits.java
@@ -0,0 +1,26 @@
+package com.karpovich.leetcode.arrays;
+
+
+/*
+Given an array nums of integers, return how many of them contain an even number of digits.
+ */
+public class FindNumbersWithEvenNumberOfDigits {
+    public int findNumbers(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+            while (nums[i] > 0) {
+                nums[i] = nums[i] / 10;
+                count++;
+            }
+            nums[i] = count;
+        }
+
+        int tmpCount = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                tmpCount++;
+            }
+        }
+        return tmpCount;
+    }
+}
---
+++ src/test/java/com/karpovich/leetcode/arrays/FindNumbersWithEvenNumberOfDigitsTest.java
@@ -0,0 +1,19 @@
+package com.karpovich.leetcode.arrays;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FindNumbersWithEvenNumberOfDigitsTest {
+
+    @Test
+    @DisplayName("Input: nums = [12,345,2,6,7896] Output: 2")
+    void findNumbers() {
+        //given
+        int[] nums = new int[]{12, 345, 2, 6, 7896};
+        //when //then
+        Assertions.assertEquals(2, new FindNumbersWithEvenNumberOfDigits().findNumbers(nums));
+    }
+}
